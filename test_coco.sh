042862088d1e4dc5cc7e57e9422aec3ea9da0771
abdelrahman-gaber <eng.abdelrahman.gaber@gmail.com>
2019-07-14 23:57:22 +0000

    small modifications

--- test_coco.sh
+++ test_coco.sh
@@ -1,6 +1,8 @@
-./darknet detector valid cfg/coco-train.data cfg/yolov3-tiny.cfg models/yolov3-tiny.weights -out coco -thresh 0.001
+#./darknet detector valid cfg/coco-train.data cfg/yolov3-tiny.cfg models/yolov3-tiny.weights -out coco -thresh 0.001
 
-#./darknet detector valid cfg/coco-train.data cfg/yolov3-tiny.cfg backup/yolov3-tiny-train_400000.weights -out coco -thresh 0.001
+# 416x416 or 320x320
+./darknet detector valid cfg/coco-train.data cfg/yolov3-tiny.cfg backup/yolov3-tiny-train_500000.weights -out coco -thresh 0.001
+
 
 python evaluate_coco.py
 
